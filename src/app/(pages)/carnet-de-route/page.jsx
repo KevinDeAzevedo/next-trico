980e96d35f2acdbbcb9a8fa60e40e6efd6b1fd46
KevinDeAzevedo <deazevedo.kevin@gmail.com>
2023-03-15 19:33:01 +0100

    country card ok

--- src/app/(pages)/carnet-de-route/page.jsx
+++ src/app/(pages)/carnet-de-route/page.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from 'next/link';
+import BotButton from '@/app/components/BotButton';
 
 const options = {
   headers: {
@@ -8,7 +10,10 @@ const options = {
 };
 
 async function getCountries() {
-  const res = await fetch(`${process.env.STRAPI_URL}/api/countries`, options);
+  const res = await fetch(
+    `${process.env.STRAPI_URL}/api/countries?populate=*`,
+    options
+  );
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error('Failed to fetch data');
@@ -32,16 +37,32 @@ export default async function Carnet() {
   const page = carnet.data;
   return (
     <main>
-      <h1>{page.title}</h1>
-      <ul>
-        {countries.map((country, index) => (
-          <li key={country.id}>
-            <Link href={`/carnet-de-route/${country.slug}`}>
-              {country.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <section className="hero">
+        <h1>{page.title}</h1>
+        <BotButton link="#liste-pays" ui="-tiny" />
+      </section>
+      <section id="liste-pays">
+        <ul className="countries">
+          {countries.map((country, index) => (
+            <li key={country.id} className="country">
+              <Link href={`/carnet-de-route/${country.slug}`}>
+                <div className="country-greeting">
+                  <p>{country.greeting}</p>
+                </div>
+                <div className="country-name">
+                  <p>{country.name}</p>
+                </div>
+                <div className="country-image">
+                  <img
+                    src={`${process.env.STRAPI_URL}${country.cover.data.url}`}
+                    alt="Cover du pays"
+                  />
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
     </main>
   );
 }
