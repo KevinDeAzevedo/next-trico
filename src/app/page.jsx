840dd51d3ad5ba1aea4d7fd77a435cdb6b58f644
KevinDeAzevedo <deazevedo.kevin@gmail.com>
2023-04-06 17:37:48 +0200

    clean

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable @next/next/no-img-element */
-import Link from 'next/link';
 import BotButton from './components/BotButton';
 import PostCard from './components/PostCard';
 import Status from './components/Status';
