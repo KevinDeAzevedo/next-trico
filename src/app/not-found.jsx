48469b6a5d398bb3c1e79b1b438d23852dc1c125
KevinDeAzevedo <deazevedo.kevin@gmail.com>
2023-04-09 10:38:43 +0200

    fix not found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,19 @@
+'use client';
+
+function handleClick(e) {
+  e.preventDefault();
+  window.location.href = '/';
+}
+
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>
+        <span>404</span>La page demandée est introuvable 😢
+      </h1>
+      <a href="/" onClick={handleClick}>
+        Retour à l'Accueil
+      </a>
+    </main>
+  );
+}
