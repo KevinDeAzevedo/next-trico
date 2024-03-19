4e38ca729453a70b953d62d0ba6373963f83b802
KevinDeAzevedo <deazevedo.kevin@gmail.com>
2024-03-19 18:47:56 +0100

    hide button and restyle

--- src/app/components/PostCard.jsx
+++ src/app/components/PostCard.jsx
@@ -1,25 +1,33 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from 'next/link';
 import When from './When';
-import Button from './Button';
+import Image from 'next/image';
+import Arrow from '../assets/Arrow-right.svg';
 
 export default function PostCard({ date, title, intro, cover, link }) {
   return (
-    <div className="postcard">
-      <div className="postcard-image">
-        <img
-          src={`${process.env.STRAPI_URL}${cover}`}
-          alt="Couverture d'article"
-        />
-      </div>
-      <div className="postcard-content">
-        <div>
-          <When date={date} />
-          <h3 className="postcard-content-title">{title}</h3>
-          <Button name="Voir plus" link={link} />
+    <Link href={link}>
+      <div className="postcard">
+        <div className="postcard-image">
+          <img
+            src={`${process.env.STRAPI_URL}${cover}`}
+            alt="Couverture d'article"
+          />
         </div>
-        <p className="postcard-content-intro">{intro}</p>
+        <div className="postcard-content">
+          <div>
+            <When date={date} />
+            <h3 className="postcard-content-title">
+              {title}
+              <span>
+                <Image src={Arrow} width={15} height={15} alt=">" />
+              </span>
+            </h3>
+            {/* <Button name="Voir plus" link={link} /> */}
+          </div>
+          <p className="postcard-content-intro">{intro}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
